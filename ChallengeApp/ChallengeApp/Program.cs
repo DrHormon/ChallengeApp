44020973a9a7522a7feb440e7620b6365ac8f656
Andrzej <hormon@mac.com>
2023-01-28 22:08:11 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// komunikatPierwszy: "Kobiet poniżej 30 lat"
+// komunikatDrugi: "Ewa, lat 33"
+// "Niepełnoletni Mężczyzna"
+
+var name = "Andrzej";
+var sex = 'M';
+var age = 54;
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == 'M' && age < 18) 
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
