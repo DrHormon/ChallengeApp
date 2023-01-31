2358a357f0db9349db6bcbf066a62d690c66288b
Andrzej <hormon@mac.com>
2023-01-31 08:26:18 +0100

    Zadanie z lekcji 45 - Częstość występwoania znaków.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,25 +1,91 @@
-﻿
-// komunikatPierwszy: "Kobiet poniżej 30 lat"
-// komunikatDrugi: "Ewa, lat 33"
-// "Niepełnoletni Mężczyzna"
+﻿// Zadanie: Obliczenie częstości występowania znaków w podanej zmiennej
+// pierwotnie określonej jako liczba typu integer 4456. To zostawiłem dla siebie "int number = 4456;"
 
-var name = "Andrzej";
-var sex = 'M';
-var age = 120;
+// Zamiast stosować zmienną typu int zastosowałem zmienną typu string. W końcu i tak problemem
+// jest policzenie częstości występowania znaków. Sposób konwersji zapamiętałem. Liczba mają ograniczenie długości
+// zmienne typu string chyba nie. Chyba bo nie sprawdzałem.
 
-if (sex == 'K' && age < 30)
+// Podanie analizowanej zmiennej
+string number = "88888888888876543";
+
+//Zdeklarowanie liczników dla każdej z występujących cyfr.
+var zerocounter = 0;
+var onecounter = 0;
+var twocounter = 0;
+var threecounter = 0;
+var fourcounter = 0;
+var fivecounter = 0;
+var sixcounter = 0;
+var sevencounter = 0;
+var eightcounter = 0;
+var ninecounter = 0;
+
+// Ponieważ zdeklarowałem analizowaną liczbę jako string to nie muszę konwertować liczby na string.
+// string numberAsString = number.ToString();
+// Pewnie mógłbym użyć zmiennej number zamiast numberAsString, ale tak jest czytelniej.
+
+string numberAsString = number;
+char [] letters = numberAsString.ToArray();
+
+// Ciekaw jestem czy ktoś czyta kod.
+// Pytanie: Czy tekst w nawiasie foreach znajdujący się po char ""letter" określa sposób
+// nazywania elementów tej tablicy? Czy mógłbym podać dowolnie wybrany?
+// Czym jest literał i czym '' różni się od ""?
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (letter == '0')
+    {
+        zerocounter++;
+    }
+    else if (letter == '1')
+    {
+        onecounter++;
+    }
+    else if (letter == '2')
+    {
+        twocounter++;
+    }
+    else if (letter == '3')
+    {
+        threecounter++;
+    }
+    else if (letter == '4')
+    {
+        fourcounter++;
+    }
+    else if (letter == '5')
+    {
+        fivecounter++;
+    }
+    else if (letter == '6')
+    {
+        sixcounter++;
+    }
+    else if (letter == '7')
+    {
+        sevencounter++;
+    }
+    else if (letter == '8')
+    {
+        eightcounter++;
+    }
+    else if (letter == '9')
+    {
+        ninecounter++;
+    }
+
 }
-else if (name == "Ewa" && age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (sex == 'M' && age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-else         
-{
-    Console.WriteLine(name + " płeć M "+"wiek: " + age);
-}
+    
+// WYpisanie obliczonych wyników. 
+    Console.WriteLine("Podana liczba to:"+number);
+    Console.WriteLine("0 - " + zerocounter); 
+    Console.WriteLine("1 - " + onecounter);
+    Console.WriteLine("2 - " + twocounter);
+    Console.WriteLine("3 - " + threecounter);
+    Console.WriteLine("4 - " + fourcounter);
+    Console.WriteLine("5 - " + fivecounter);
+    Console.WriteLine("6 - " + sixcounter);
+    Console.WriteLine("7 - " + sevencounter);
+    Console.WriteLine("8 - " + eightcounter);
+    Console.WriteLine("9 - " + ninecounter);
